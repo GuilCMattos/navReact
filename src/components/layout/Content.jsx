3d9cdb19798be43a72c0ee2409e6333933005388
Guilherme <mttsguil@gmail.com>
2022-09-14 11:10:47 -0300

    add: react router works

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,18 @@
 import './Content.css'
 import React from "react";
+import {Routes, Route} from 'react-router-dom'
+
+import About from '../../views/examples/About';
+import Home from '../../views/examples/Home';
 
 export default function Content(props) { 
     return (
         <main className="Content">
-            <h1>Componente Content</h1>
+
+            <Routes>
+                <Route path='/about' element={<About/>} />
+                <Route path='/' element={<Home />} />
+            </Routes>
         </main>
     )
 }
--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -1,17 +1,19 @@
 import './Menu.css'
 import React from "react";
 
+import { Link } from 'react-router-dom'
+
 export default function Menu(props) { 
     return (
         <aside className="Menu">
             <nav>
                 <ul>
                     <li>
-                        <a href="#">Home</a>
+                        <Link to="/">Home</Link>
                     </li>
 
                     <li>
-                        <a href="#">About</a>
+                        <Link to="/About">About</Link>
                     </li>
                 </ul>
             </nav>
