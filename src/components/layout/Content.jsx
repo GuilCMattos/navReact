df306280c37b44308df9b628ab431184bfee6b61
Guilherme <mttsguil@gmail.com>
2022-09-14 11:25:34 -0300

    add: not found page

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import {Routes, Route} from 'react-router-dom'
 import About from '../../views/examples/About';
 import Home from '../../views/examples/Home';
 import Param from '../../views/examples/Param';
+import NotFound from '../../views/examples/NotFound';
 
 export default function Content(props) { 
     return (
@@ -14,6 +15,7 @@ export default function Content(props) {
                 <Route path='/about' element={<About/>} />
                 <Route path='/' element={<Home />} />
                 <Route path='/param/:id' element={<Param />} />
+                <Route path='*' element={<NotFound />} />
             </Routes>
         </main>
     )
---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function notFound(props) { 
+    return (
+        <div className="notFound">
+            <h1>404</h1>
+            <p>Ops... Página não encontrada</p>
+        </div>
+    )
+}
